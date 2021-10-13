472494d4b63de5c40dde0407a2beada1e5ba136f
mike <mikechang0422@gmail.com>
2021-10-14 01:45:51 +0800

    增加文字模仿電池充電的效果 並使用clamp這個新css設定 clamp(最小顯示值,中間範圍,最大顯示值)

--- js/style.js
+++ js/style.js
@@ -8,3 +8,25 @@ function scrolledHandler (){
     section2H2.style.setProperty('--percentage',`${scrolled * 100}%`)
 }
 document.addEventListener('scroll',scrolledHandler)
+
+
+//section3
+let progress = 0
+function run () {
+    if (progress < 1){
+        progress = parseFloat((progress + 0.01).toFixed(2))//toFixed 取小數點2位 parseFloat 字串轉數字
+
+        document.querySelector('.battery__font').style.setProperty('--progress',progress)//setProperty 將progress設定到css變數
+
+        setTimeout(run,20)//每20毫秒執行一次 run 等於 執行一次 + 0.01 +到1為止
+    }
+}
+
+document.querySelector('#empty').addEventListener('click', (e) => {
+    progress = 0
+    document.querySelector('.battery__font').style.setProperty('--progress', progress)
+})
+  
+document.querySelector('#full').addEventListener('click', (e) => {
+    run()
+})
